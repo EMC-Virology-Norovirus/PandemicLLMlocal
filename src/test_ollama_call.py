6a076e15efd60ff80eb5aeff9a6f2dfa95951147
ehulland <e.frame@erasmusmc.nl>
2026-02-25 15:26:10 +0100

    "updating data file locations"

--- src/test_ollama_call.py
+++
@@ -1,7 +0,0 @@
-import ollama, json, traceback
-try:
-    resp = ollama.chat(model='mistral', messages=[{'role':'user','content':'Hello from test script'}])
-    print(json.dumps(resp, indent=2))
-except Exception as e:
-    traceback.print_exc()
-    print('EXCEPTION_STR:', repr(e))
